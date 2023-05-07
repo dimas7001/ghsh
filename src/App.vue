1b09ba10e8a03333417593618b0a6e33157f3227
Dmytro Baranov <ba.dm.ig.bdi@gmail.com>
2023-05-07 20:14:25 +0200

    fonts added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+    integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="sass">
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-Bold.ttf")
+  font-weight: 700
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-SemiBold.ttf")
+  font-weight: 600
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-Medium.ttf")
+  font-weight: 500
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-Regular.ttf")
+  font-weight: 400
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-Light.ttf")
+  font-weight: 300
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-ExtraLight.ttf")
+  font-weight: 200
+  font-style: normal
+  font-display: swap
+@font-face
+  font-family: "JosefinSans"
+  src: url("assets/fonts/JosefinSans/JosefinSans-Thin.ttf")
+  font-weight: 100
+  font-style: normal
+  font-display: swap
+*
+  box-sizing: border-box
+[v-cloak]
+  display: none
 </style>
